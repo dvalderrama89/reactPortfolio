12ec7d306319ec68037827b4a099084b802529b7
David Valderrama <dvalderrama08@gmail.com>
2021-08-29 03:02:09 -0700

    Added nav component to header component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,31 +1,15 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
 } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import Header from './components/Header';
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <Router>
-        <Header />
-        <Switch>
-          <Route path="/contact">
-            <Contact />
-          </Route>  
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        <Footer />
+      <Header />
+      <Footer />
     </Router>
   );
 }
--- react-portfolio/src/components/Navigation/index.js
+++ react-portfolio/src/components/Navigation/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import {
+    Switch,
+    Route,
+    Link,
+  } from "react-router-dom";
+  import Home from '../../pages/Home';
+  import About from '../../pages/About';
+  import Contact from '../../pages/Contact';
+  import resume from '../../david_valderrama_resume.pdf';
 
 export default function Navigation() {
   return (
     <>
-    </>
+        <nav>
+            <div className="nav-wrapper teal darken-3">
+            <Link to="/" className="brand-logo">Home</Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                <a class="grey-text text-lighten-3" download href={resume}>Resume</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>  
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+  </>
   );
 }
